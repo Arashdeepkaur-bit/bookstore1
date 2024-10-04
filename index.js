9e8c251be38033317ced911db47f4933e093e5c3
Arash <sainiarash555@gmail.com>
2024-10-04 13:02:49 +0530

    bookstore2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import bookRoute from "./routes/.route.book";
-import userRoute from "./routes/routes.user";
+// import bookRoute from "./routes/.route.book";
+// import userRoute from "./routes/routes.user";
 const app=express();
 app.use(core());
 app.use(express());
@@ -18,8 +18,10 @@ try{
     console.log("connected to mongodb");
 }catch(error){
     console.log("error:",error);
-}app.use("/book",bookRoute);
-app.use("/user",userRoute);
+}
+// app.use("/book",bookRoute);
+// app.use("/user",userRoute);
+
 app.listen(PORT,()=>{
     console.log(`server is listening on port ${PORT}`);
 });
