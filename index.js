ae8f17b17c1c38c295744879f2b28ff8a1d86dae
Arash <sainiarash555@gmail.com>
2024-10-04 11:45:06 +0530

    bookstore2

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from  "cors";
+import bookRoute from "./routes/.route.book";
+import userRoute from "./routes/routes.user";
+const app=express();
+app.use(core());
+app.use(express());
+dotenv.config();
+const PORT =process.env.PORT || 4000;
+const URL =process.env.MongoDBURI;
+try{
+    mongoose.connect(URL,{
+      userNewUrlParser:true,
+      useUnfiedTopology:true,  
+    });
+    console.log("connected to mongodb");
+}catch(error){
+    console.log("error:",error);
+}app.use("/book",bookRoute);
+app.use("/user",userRoute);
+app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`);
+});
